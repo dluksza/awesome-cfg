eaf7198ddca1bbb498f7b6fd00f69a24e45e5594
Dariusz Luksza <dariusz@luksza.org>
2011-12-15 22:02:51 +0100

    Merged with default awesome v3.4.11 config

--- rc.lua
+++ rc.lua
@@ -7,10 +7,34 @@ require("awful.rules")
 require("beautiful")
 -- Notification library
 require("naughty")
-
 require("vicious")
 
--- require("lfs-run_once")
+-- {{{ Error handling
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
+if awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = awesome.startup_errors })
+end
+
+
+-- Handle runtime errors after startup
+do
+    local in_error = false
+    awesome.add_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = err })
+        in_error = false
+    end)
+end
+-- }}}
+
 
 local capi = {
 	screen = screen
